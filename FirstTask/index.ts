c1af53c7c3996c082c0a6a46e2ecd8f3ae596e22
manihateu <nik862.nikita@mail.ru>
2023-06-15 13:34:21 +0300

    FirstTask

--- FirstTask/index.ts
+++ FirstTask/index.ts
@@ -1,11 +1,9 @@
-//type TObj = { key1: number };
-//const a: TObj = { key1: 0 };
-//const keys = ["key2"];
-//console.log(a[keys[0]]); // Получаем undefined - нужно что бы не могли
-// и ругался TS
-
-//Решение
 type TObj = { key1: number };
 const a: TObj = { key1: 0 };
-const keys = ["key2"];
-console.log(a[keys[0] as keyof TObj]);
+const keys = ["key2"] as const;
+
+type Keys = typeof keys[number];
+type IndexedObj = { [key in Keys]: number };
+
+const indexedA: IndexedObj = a; // Присваиваем `a` переменной с индексным типом
+console.log(indexedA[keys[0]]); // Теперь TypeScript ругается, так как ключ `key2` отсутствует в типе IndexedObj
