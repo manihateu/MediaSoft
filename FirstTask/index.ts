928fdb712d46546dc7566841c5cdccac0c8134fd
manihateu <102178609+manihateu@users.noreply.github.com>
2023-06-19 13:43:23 +0300

    FirstTaskUpdate

--- FirstTask/index.ts
+++ FirstTask/index.ts
@@ -1,7 +1,5 @@
 type TObj = { key1: number };
 const a: TObj = { key1: 0 };
-const keys = ["key2"] as const;
+const keys: Array<keyof TObj> = ["key2"];
 
-type Keys = (typeof keys)[number];
-
-const indexedA: TObj[Keys] = a[keys];
+console.log(a[keys[0]]); 
