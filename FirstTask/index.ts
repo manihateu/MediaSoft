36c3dc6ffd45e0ffc1a19a3b997ffa0fe841ffe0
manihateu <102178609+manihateu@users.noreply.github.com>
2023-06-19 12:43:17 +0300

    uodateFirstTask

--- FirstTask/index.ts
+++ FirstTask/index.ts
@@ -2,8 +2,6 @@ type TObj = { key1: number };
 const a: TObj = { key1: 0 };
 const keys = ["key2"] as const;
 
-type Keys = typeof keys[number];
-type IndexedObj = { [key in Keys]: number };
+type Keys = (typeof keys)[number];
 
-const indexedA: IndexedObj = a; // Присваиваем `a` переменной с индексным типом
-console.log(indexedA[keys[0]]); // Теперь TypeScript ругается, так как ключ `key2` отсутствует в типе IndexedObj
+const indexedA: TObj[Keys] = a[keys];
